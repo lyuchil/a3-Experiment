d49a29e87429fa92f802b2a8c89e118b6c0bce91
lfoley7 <luke.c.foley@gmail.com>
2024-03-13 17:12:26 -0400

    live hosting specs

--- server.js
+++ server.js
@@ -5,35 +5,38 @@ const { google } = require('googleapis');
 
 // Initialize the Google Sheets API client
 const auth = new google.auth.GoogleAuth({
-  keyFile: './config/data-vis-project-3-80be54215644.json',
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    credentials: {
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-const spreadsheetId = '1XE-RJOsJrHnWL7jkX1MfWSZSIXIi0G1YO_1qSIKj1Xg';
+const spreadsheetId = process.env.SPREADSHEET_ID;
 
 
 async function appendData(guessedValueArray, correctValueArray) {
-  const authClient = await auth.getClient();
-  const googleSheets = google.sheets({ version: 'v4', auth: authClient });
+    const authClient = await auth.getClient();
+    const googleSheets = google.sheets({ version: 'v4', auth: authClient });
 
-  const range = 'Sheet1';
-  const valueInputOption = 'RAW';
-  const values = [[...guessedValueArray, ...correctValueArray]];
+    const range = 'Sheet1';
+    const valueInputOption = 'RAW';
+    const values = [[...guessedValueArray, ...correctValueArray]];
 
-  const request = {
-    spreadsheetId,
-    range,
-    valueInputOption,
-    resource: { values },
-  };
+    const request = {
+        spreadsheetId,
+        range,
+        valueInputOption,
+        resource: { values },
+    };
 
-  try {
-    await googleSheets.spreadsheets.values.append(request);
-    return { status: 'Success' };
-  } catch (err) {
-    console.error('The API returned an error: ' + err);
-    throw err;
-  }
+    try {
+        await googleSheets.spreadsheets.values.append(request);
+        return { status: 'Success' };
+    } catch (err) {
+        console.error('The API returned an error: ' + err);
+        throw err;
+    }
 }
 
 // Set up the Express server
@@ -44,15 +47,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/append-data', async (req, res) => {
-  const { guessedValueArray, correctValueArray } = req.body;
-  try {
-    const result = await appendData(guessedValueArray, correctValueArray);
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
-  }
+    const { guessedValueArray, correctValueArray } = req.body;
+    try {
+        const result = await appendData(guessedValueArray, correctValueArray);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
